ebe94239445689dbcb6f5c4bb0bc60615ea3b74a
rebecab27 <151066826+rebecab27@users.noreply.github.com>
2024-05-13 16:21:01 +0000

    Deploying to gh-pages from @ rebecab27/goit-js-hw-09@d256fc22e218c7200561b35206cdcf62dc773d3b 🚀

---
+++ 01-color-switcher.8577cd58.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),a=document.body;e.addEventListener("click",(function(){e.disabled=!0,t=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),n.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.8577cd58.js.map
